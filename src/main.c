fe7063a8256b956b8372594262d9e6d472d6e979
chknoflach <christian@kno.at>
2026-01-02 09:06:45 +0100

    refactored settings and prepared physics objects collection

--- src/main.c
+++ src/main.c
@@ -4,65 +4,92 @@
 
 #define FPS 240
 #define FPS_PHYS 120
+#define MEM_STEP 64
 
 typedef struct {
-	float mass;		// mass in kg
-	float drag;		// drag coefficient
-	float g_tweak;  // manual gravity tweak; 1.0f = normal
-	Vector2 v;		// m/s
-	Vector2 pos;	// meters
+    float   mass;       // mass in kg
+    float   drag;       // drag coefficient
+    float   g_tweak;    // manual gravity tweak; 1.0f = normal
+    Vector2 v;          // m/s
+    Vector2 pos;        // meters
 } physics_body;
+
+typedef struct {
+    physics_body    *items;
+    int             count;
+    int             capacity;
+} physics_collection;
+
+typedef struct {
+    float   g;
+    int     g_px;
+    int     fps;
+    float   dt;
+    float   dt_acc;
+} physics_settings;
+
+typedef struct {
+    int     width;
+    int     height;
+    int     fps;
+} gfx_settings;
 
 int main(void)
 {
-	const int WIDTH = 800;
-	const int HEIGHT = 600;
-	const float G = 9.81f;
-	const int G_PX = G * 5500.0f;
-	const float phys_dt = 1.0f / FPS_PHYS;
-	float phys_dt_acc = 0.0f;
-	float dt;
-	physics_body bd;
-	
-	bd.mass = 10.0f;
-	bd.drag = 0.02f;
-	bd.g_tweak = 1.0f;
-	bd.pos.x = WIDTH / 2;
-	bd.pos.y = 0.0f;
-	bd.v.x = 0.0f;
-	bd.v.y = 0.0f;
+    gfx_settings gfx = {0};
+    physics_settings phys = {0};
 
-    InitWindow(WIDTH, HEIGHT, "raylib test");
-	SetTargetFPS(FPS);
+    gfx.width = 800;
+    gfx.height = 600;
+    gfx.fps = 60;
 
-	while (!WindowShouldClose())
+    phys.g = 9.81f;
+    phys.g_px = phys.g * 5000.0f;
+    phys.fps = 120;
+    phys.dt = 1.0f / phys.fps;
+
+    float dt;
+    physics_body bd;
+    
+    bd.mass = 10.0f;
+    bd.drag = 0.02f;
+    bd.g_tweak = 2.0f;
+    bd.pos.x = gfx.width / 2;
+    bd.pos.y = 0.0f;
+    bd.v.x = 0.0f;
+    bd.v.y = 0.0f;
+
+    InitWindow(gfx.width, gfx.height, "raylib test");
+    SetTargetFPS(gfx.fps);
+
+    while (!WindowShouldClose())
     {
-		dt = GetFrameTime();
-		phys_dt_acc += dt;
+        dt = GetFrameTime();
+        phys.dt_acc += dt;
 
-		while (phys_dt_acc >= phys_dt)
-		{
-			float acc_y = (G_PX * bd.g_tweak)
-				- (bd.drag / bd.mass)
-					* bd.v.y * fabsf(bd.v.y);
-	
-	        bd.v.y += acc_y * phys_dt;
-	        bd.pos.y += bd.v.y * phys_dt;
+        while (phys.dt_acc >= phys.dt)
+        {
+            float acc_y = (phys.g_px * bd.g_tweak)
+                - (bd.drag / bd.mass)
+                    * bd.v.y * fabsf(bd.v.y);
+    
+            bd.v.y += acc_y * phys.dt;
+            bd.pos.y += bd.v.y * phys.dt;
 
-			if (bd.pos.y > HEIGHT)
-			{
-				bd.v.y = bd.pos.y = 0.0f;
-				bd.pos.x += 2.0f;
-				if (bd.pos.x > WIDTH)
-					bd.pos.x = 0.0f;
-				bd.mass += 1.0f;
-			}
+            if (bd.pos.y > gfx.height)
+            {
+                bd.v.y = bd.pos.y = 0.0f;
+                bd.pos.x += 2.0f;
+                if (bd.pos.x > gfx.width)
+                    bd.pos.x = 0.0f;
+                bd.mass += 1.0f;
+            }
 
-			phys_dt_acc -= phys_dt;
-		}
+            phys.dt_acc -= phys.dt;
+        }
 
-		BeginDrawing();
-		DrawPixel(bd.pos.x, bd.pos.y, RED);
+        BeginDrawing();
+        DrawPixel(bd.pos.x, bd.pos.y, RED);
         EndDrawing();
     }
     CloseWindow();
